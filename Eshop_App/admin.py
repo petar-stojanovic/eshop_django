59795f3ba030a1a9b75fed0d934b0b67409d5133
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-06-17 14:30:32 +0200

    created models

--- Eshop_App/admin.py
+++ Eshop_App/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import Category, Component
 
 # Register your models here.
+admin.site.register(Category)
+
+
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "platform", "image_display")
+
+    def image_display(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-width:50px;max-height:50px" />')  # Renders the image
+
+    image_display.short_description = 'Image'
+
+
+admin.site.register(Component, ComponentAdmin)
--- Eshop_App/models.py
+++ Eshop_App/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Component(models.Model):
+    PLATFORM_CHOICES = [
+        ('desktop', 'Desktop'),
+        ('laptop', 'Laptop'),
+    ]
+
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    platform = models.CharField(max_length=10, choices=PLATFORM_CHOICES, default="desktop")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
