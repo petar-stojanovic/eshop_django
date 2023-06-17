4d5583310516c02c1469304eae59f64469bb3cc1
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-06-17 19:44:31 +0200

    added login and logout functionality

--- members/urls.py
+++ members/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name="index"),
+    path('login_user', views.login_user, name="login"),
+    path('logout_user', views.logout_user, name="logout"),
 
 ]
