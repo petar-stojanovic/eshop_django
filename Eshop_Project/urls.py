c18bff98bd3fc723b0f1dc0d26aff2fa2875e77e
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-06-17 17:05:45 +0200

    added bootstrap locally. added navbar

--- Eshop_Project/urls.py
+++ Eshop_Project/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Eshop_App import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.index, name="index"),
+
+                  # auth
+                  path('accounts/', include('django.contrib.auth.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
