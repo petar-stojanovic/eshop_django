01031377fee7dc5e7f285c9cacfedcdfb9568f54
petar-stojanovic <2001petarstojanovic@gmail.com>
2023-06-17 17:42:06 +0200

    created members app

--- Eshop_Project/settings.py
+++ Eshop_Project/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Eshop_App'
+    'Eshop_App',
+    'members',
 ]
 
 MIDDLEWARE = [
--- Eshop_Project/urls.py
+++ Eshop_Project/urls.py
@@ -18,13 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from Eshop_App import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', views.index, name="index"),
+                  path('', include('Eshop_App.urls')),
 
                   # auth
-                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('members/', include('django.contrib.auth.urls')),
+                  path('members/', include('members.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
